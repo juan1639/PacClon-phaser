9c082a4ee41861fc6faea27ffae2666176e31e24
juan <juaneguiabad1639@gmail.com>
2023-12-16 12:59:14 +0100

    cambiado archivo components/jugadorjs por jugador-copiajs

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -4,7 +4,7 @@
 // -----------------------------------------------------------------------------------------
 import { loader } from './loader.js';
 import { Laberinto } from '../components/laberinto.js';
-import { Jugador } from './../components/jugador.js';
+import { Jugador } from './../components/jugador-copia.js';
 
 const WIDTH = 800;
 const HEIGHT = 550;
@@ -34,8 +34,8 @@ export class Game extends Phaser.Scene {
     // this.gameoverImage = this.add.image(400, 90, 'gameover');
     // this.gameoverImage.visible = false;
     
-    this.cameras.main.setBounds(0, 0, Math.floor(800 * 1.5), Math.floor(550 * 1.6));
-    this.physics.world.setBounds(0, 0, Math.floor(800 * 1.5), Math.floor(550 * 1.6));
+    this.cameras.main.setBounds(0, 0, Math.floor(800 * 1.45), Math.floor(550 * 1.6));
+    this.physics.world.setBounds(0, 0, Math.floor(800 * 1.45), Math.floor(550 * 1.6));
 
     this.laberinto.create();
     this.jugador.create();
@@ -45,6 +45,9 @@ export class Game extends Phaser.Scene {
 
     // this.physics.add.collider(this.jugador.get(), this.plataforma.get(), this.platformaImpacto, null, this);
     // this.physics.add.collider(this.jugador.get(), this.laberinto.get(), (jug, plat) => {console.log(jug.body.touching.right);});
+    /* this.physics.add.collider(this.jugador.get(), this.laberinto.get(), (jugador, laberinto) => {
+      console.log(jugador.touching.up);
+    }); */
   }
 
   // ================================================================
